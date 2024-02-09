fe1c807e91a317c733e9939197ca6771832e797e
ananathmukesh <154673797+ananathmukesh@users.noreply.github.com>
2024-02-09 16:06:00 +0530

    uncommend live url

--- src/config/ServerUrl.js
+++ src/config/ServerUrl.js
@@ -1,21 +1,21 @@
-const serverUrl = "http://localhost:8001/api";
-const chatserverUrl = "http://localhost:8001/chat";
+// const serverUrl = "http://localhost:8001/api";
+// const chatserverUrl = "http://localhost:8001/chat";
 
-const livenodeUrl = "https://backendnode-io40.onrender.com";
+// const livenodeUrl = "https://backendnode-io40.onrender.com";
 
-const senderreceiverImg = "http://localhost:8001/assets/SndRcvdImage/";
-const senderreceiverVideo = "http://localhost:8001/assets/SndRcvdVideo";
-const senderreceiverDoc = "http://localhost:8001/assets/SndRcvdPDF";
+// const senderreceiverImg = "http://localhost:8001/assets/SndRcvdImage/";
+// const senderreceiverVideo = "http://localhost:8001/assets/SndRcvdVideo";
+// const senderreceiverDoc = "http://localhost:8001/assets/SndRcvdPDF";
 
 
-// const serverUrl = "https://backend-wc4l.onrender.com/api";
-// const chatserverUrl = "https://backend-wc4l.onrender.com/chat";
+const serverUrl = "https://backend-v.onrender.com//api";
+const chatserverUrl = "https://backend-v.onrender.com//chat";
 
-// const livenodeUrl = "https://backend-wc4l.onrender.com/";
+const livenodeUrl = "https://backend-v.onrender.com//";
 
-// const senderreceiverImg = "https://backend-wc4l.onrender.com/assets/SndRcvdImage/";
-// const senderreceiverVideo = "https://backend-wc4l.onrender.com/assets/SndRcvdVideo";
-// const senderreceiverDoc = "https://backend-wc4l.onrender.com/assets/SndRcvdPDF";
+const senderreceiverImg = "https://backend-v.onrender.com//assets/SndRcvdImage/";
+const senderreceiverVideo = "https://backend-v.onrender.com//assets/SndRcvdVideo";
+const senderreceiverDoc = "https://backend-v.onrender.com//assets/SndRcvdPDF";
 
 export { 
     serverUrl, 
